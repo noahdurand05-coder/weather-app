fdd05a7c13b15e8e3692f9780a5a49df5fb5222a
Noah <noah.durand@campus-eni.fr>
2026-06-19 00:24:01 +0200

    Latest modif

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/* Récupération des coordonnées GPS de l'utilisateur si il le souhaite*/
+/* ======================= Récupération des coordonnées GPS de l'utilisateur si il le souhaite ====================================*/ 
 
 
 navigator.geolocation.getCurrentPosition(
@@ -20,31 +20,10 @@ navigator.geolocation.getCurrentPosition(
   }
 );
 
-const ville = document.getElementById("search-bar")
-if(ville){
-    ville.addEventListener("click", )
-}
 
 
-async function searchVille(){
+/* ======================= Récupération des données météos en fonctions des données utilisateur ====================================*/ 
 
-    
-
-    const urlVille =`https://geocoding-api.open-meteo.com/v1/search?name=${ville}`
-
-    const reponse = await fetch(urlVille)
-    const data = await reponse.json()
-
-    console.log(data)
-
-}
-
-
-
-
-
-
-/* Récupération des données météo en fonction de ma longitude et latitude*/
 
 async function appelAPI (latitude, longitude){
 
@@ -53,10 +32,73 @@ async function appelAPI (latitude, longitude){
 
     const reponse = await fetch(urlAPILongLat)
     const data = await reponse.json()
+    const temperature = data.current.temperature_2m
     
     console.log(data)
+
+    affichMeteo(temperature)
 
 }
 
 
+/* ======================= Récupération de la saisie utilisateur avec geocoding ===================================================*/ 
 
+const ville = document.getElementById("search-bar")
+if(ville){
+    ville.addEventListener("change", rechercheVille)
+      
+}
+    async function rechercheVille() {    
+      const input = document.getElementById("search-bar").value
+      console.log(input)
+
+    const urlVille =`https://geocoding-api.open-meteo.com/v1/search?name=${input}`
+
+    const reponse = await fetch(urlVille)
+    const data = await reponse.json()
+
+          if(data.results != undefined){
+    const latitude = data.results[0].latitude
+    const longitude = data.results[0].longitude
+      console.log(data.results[0])
+      appelAPI(latitude, longitude)
+    }else {
+      console.log('Erreur Veuillez réessayer')
+      affichErreur()
+    }
+        
+  }
+
+
+
+
+/* ======================= Fonction Affichage de la météo ===================================================*/ 
+
+
+function affichMeteo(temperature){
+  
+  const meteoBox = document.getElementById("meteo-container")
+  const temperatureBox = document.getElementById("temperature")
+  const hygroBox = document.getElementById("hygro")
+
+
+  meteoBox.innerHTML = ""
+  
+
+  const img = document.createElement('img')
+  img.src = "Assets/Thermomètre.png";
+  img.alt = "Logo Thermomètre"
+
+  const titre = document.createElement('h2')
+  titre.textContent = "Température : " + temperature + "°C"
+
+}
+
+
+/* ======================= Fonction Affichage en cas d'erreur ===================================================*/ 
+
+function afficherErreur(){
+
+
+
+}
